33d31440c3e1a245c7219958bb458b695e2c6d55
Guilherme <guilherme.moraes-ext@ab-inbev.com>
2023-01-31 22:38:50 -0300

    :sparkles: : removed console.log

--- src/components/ProtectedLayout/index.tsx
+++ src/components/ProtectedLayout/index.tsx
@@ -2,7 +2,7 @@ import { useAuth } from "../../context/AuthProvider/useAuth";
 
 export const ProtectedLayout = ({ children }: { children: JSX.Element }) => {
   const auth = useAuth();
-  console.log("caraio");
+
   if (!auth.email) {
     return <h1>Você não possui acesso</h1>;
   }
